065ed3282da3de19bfaaca133a6822239471fdca
Alexa Karp <alexakarp@hotmail.com>
2022-05-10 23:55:46 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove();
+
+let newHeader = document.createElement('h1#victory');
+
+newHeader.id ='victory';
+
+newHeader.textContent = "Alexa is the Champion";
+document.append(newHeader);
